a6906962e52c6f5343d69ccd2510236dea79b4ef
Jonathan McArthur <silverstagecoach@gmail.com>
2022-07-05 15:40:34 -0500

    final commit

--- server/index.js
+++ server/index.js
@@ -6,5 +6,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(4000, ()=> console.log('Server is running on 4000'));
+app.get('/api/users', (req, res) => {
+    let friends = ['Nitin', 'Eric', 'Jeddy Hwang', 'Cameron', 'Riley', 'Alec PrettyMAN', 'Jack Donnell', 'Jackson Porter'];
+    res.status(200).send(friends)
+})
 
+app.get('/weather/:temperature', (req,res)=> {
+    const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
+    res.status(200).send(phrase);
+})
+
+
+
+app.listen(4000, ()=> console.log('Server is running on 4000'));
